0f4d0e9707e03b9b5291ac372d0d5b088138283e
nikita <nikitakerzhik@gmail.com>
2024-10-14 17:51:36 +1000

    edit components card and HowIt

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,19 @@
+import styles from "@/styles/Card.module.css";
 
-export function Card() {
+type ContactCardProps = {
+    text: string;
+    number: string;
+    imgSrc: string;
+};
+
+export const Card: React.FC<ContactCardProps> = ({ text, number, imgSrc }) => {
     return (
-        <>
-        </>
-    )
-}
+        <div className={styles.card}>
+            <div className={styles.iconTextContainer}>
+                <img src={imgSrc} alt="message" className={styles.icon}/>
+            </div>
+            <span className={styles.number}>{number}</span>
+            <p className={styles.text}>{text}</p>
+        </div>
+    );
+};
--- src/components/HowIt.tsx
+++ src/components/HowIt.tsx
@@ -1,8 +1,13 @@
 import styles from "@/styles/HowIt.module.css"
+import {Card} from "@/components/Card";
 
 export function HowIt() {
     return (
-        <>
-        </>
+        <div className={styles.howIt}>
+            <div className={styles.howItHeader}>
+                <span>Как это происходит?</span>
+            </div>
+            <Card text="Связь с нами" number="01" imgSrc="icons/message.png"/>
+        </div>
     )
 }
