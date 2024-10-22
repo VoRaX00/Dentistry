20e1505fd05d73fc2cd3b9de2aaf4b22fb7e92c1
nikita <nikitakerzhik@gmail.com>
2024-10-22 20:59:11 +1000

    adaptive heading

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -9,7 +9,7 @@ export function Heading() {
           <img src="logo.png" alt="logo" />
           <div className={styles.contact}>
             <p>
-              +7 996 385 52 20
+              <span>+7 996 385 52 20</span>
               <Button color="#45494D" text="Связаться с нами" borderColor="#45494D" isBold={true} fontSize="22px"/>
             </p>
           </div>
